0c4c592989f3990d9f5de2d105da1cc79c3946d7
brenoscominato <brenocominato@hotmail.com>
2026-05-21 23:29:15 -0300

    ListaAtualizadas

--- CursoPython/DesempacotamentoLista.py
+++ CursoPython/DesempacotamentoLista.py
@@ -1,8 +1,29 @@
-# Desempacotamento em chamadas
-# de métodos e funções
-string = 'ABCD'
-lista = ['Maria', 'Helena', 1, 2, 3, 'Eduarda']
-tupla = 'Python', 'é', 'legal'
+# # Desempacotamento em chamadas
+# # de métodos e funções
+# string = 'ABCD'
+# lista = ['Maria', 'Helena', 1, 2, 3, 'Eduarda']
+# tupla = 'Python', 'é', 'legal'
+# salas = [
+#     # 0        1
+#     ['Maria', 'Helena', ],  # 0
+#     # 0
+#     ['Elaine', ],  # 1
+#     # 0       1       2
+#     ['Luiz', 'João', 'Eduarda', ],  # 2
+# ]
+
+# # p, b, *_, ap, u = lista
+# # print(p, u, ap)
+
+# # print('Maria', 'Helena', 1, 2, 3, 'Eduarda')
+# # print(*lista)
+# # print(*string)
+# # print(*tupla)
+
+# print(*salas, sep='\n')
+
+
+ListaTeste = ['Breno', 'Nathalia', 'Ellie', 1,2,3,4]
 salas = [
     # 0        1
     ['Maria', 'Helena', ],  # 0
@@ -12,12 +33,8 @@ salas = [
     ['Luiz', 'João', 'Eduarda', ],  # 2
 ]
 
-# p, b, *_, ap, u = lista
-# print(p, u, ap)
+a, b, c, *_, d = ListaTeste
 
-# print('Maria', 'Helena', 1, 2, 3, 'Eduarda')
-# print(*lista)
-# print(*string)
-# print(*tupla)
-
+print(*_)
+print(*ListaTeste)
 print(*salas, sep='\n')

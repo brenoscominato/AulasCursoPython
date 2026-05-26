dfd5c06053b52398607deb8b7acc523c3074c8ec
brenoscominato <brenocominato@hotmail.com>
2026-05-26 00:17:08 -0300

    Commit_cpf

--- CursoPython/Exercicio_CPF.py
+++ CursoPython/Exercicio_CPF.py
@@ -19,11 +19,21 @@ while True:
         resultado_multiplicacao += lista_contada * int(lista_cpf_contada)
 
     if resultado_multiplicacao % 11 < 2:
+        primeiro_digito = 0
         print('O primeiro digito verificador é: 0')
     else:
-        print(f'O primeiro digito verificador é: {11 - (resultado_multiplicacao % 11)}')
+        primeiro_digito = 11 - (resultado_multiplicacao % 11)
+        print(f'O primeiro digito verificador é: {primeiro_digito}')
 
-    for indice, numero_cpf in enumerate(cpf, start=11):
+    resultado_multiplicacao = 0
+    contagem = 11
+    lista_contagem = []
+    lista_cpf = []
+
+    # Inclui os 9 dígitos + o primeiro dígito verificador
+    cpf_com_primeiro_digito = cpf + str(primeiro_digito)
+    
+    for indice, numero_cpf in enumerate(cpf_com_primeiro_digito):
         lista_cpf.append(numero_cpf)
         lista_contagem.append(contagem)
         contagem -= 1

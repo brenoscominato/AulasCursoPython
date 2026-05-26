690bf15b1572027b469d9881d611f585b4dc2ee5
brenoscominato <brenocominato@hotmail.com>
2026-05-25 23:54:36 -0300

    Commit_CPF

--- CursoPython/Exercicio_CPF.py
+++ CursoPython/Exercicio_CPF.py
@@ -3,6 +3,7 @@ while True:
     contagem = 10
     lista_contagem = []
     lista_cpf = []
+    resultado_multiplicacao = 0
 
     if len(cpf) !=9:
         print('CPF deve conter 9 digitos')
@@ -13,5 +14,30 @@ while True:
         lista_contagem.append(contagem)
         contagem -= 1
 
-    print(*lista_contagem)
-    print(*lista_cpf)
+    for lista_contada, lista_cpf_contada in zip(lista_contagem, lista_cpf):
+        #print(f'{lista_contada} x {lista_cpf_contada}')
+        resultado_multiplicacao += lista_contada * int(lista_cpf_contada)
+
+    if resultado_multiplicacao % 11 < 2:
+        print('O primeiro digito verificador é: 0')
+    else:
+        print(f'O primeiro digito verificador é: {11 - (resultado_multiplicacao % 11)}')
+
+    for indice, numero_cpf in enumerate(cpf, start=11):
+        lista_cpf.append(numero_cpf)
+        lista_contagem.append(contagem)
+        contagem -= 1
+    
+    for lista_contada, lista_cpf_contada in zip(lista_contagem, lista_cpf):
+        #print(f'{lista_contada} x {lista_cpf_contada}')
+        resultado_multiplicacao += lista_contada * int(lista_cpf_contada)
+
+    if resultado_multiplicacao % 11 < 2:
+        print('O segundo digito verificador é: 0')
+    else:
+        print(f'O segundo digito verificador é: {11 - (resultado_multiplicacao % 11)}')
+
+    # print(*lista_contagem)
+    # print(*lista_cpf)
+    print(f'Resultado da multiplicação: {resultado_multiplicacao}')
+    break

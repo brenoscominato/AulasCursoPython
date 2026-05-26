3bcd01141d7d8b0bad22d446d26306235f038dcd
brenoscominato <brenocominato@hotmail.com>
2026-05-26 00:32:03 -0300

    Commit_cpf

--- CursoPython/Exercicio_CPF.py
+++ CursoPython/Exercicio_CPF.py
@@ -1,15 +1,20 @@
 while True:
-    cpf = input('Digite os nove primeiros digitos do seu cpf: ')
+    cpf = input('Digite os 11 dígitos do seu CPF: ')
     contagem = 10
     lista_contagem = []
     lista_cpf = []
     resultado_multiplicacao = 0
 
-    if len(cpf) !=9:
-        print('CPF deve conter 9 digitos')
+    if len(cpf) != 11:
+        print('CPF deve conter 11 dígitos')
         continue
+    
+    # Separa os 9 dígitos e os 2 dígitos verificadores informados
+    cpf_numeros = cpf[:9]
+    primeiro_digito_informado = int(cpf[9])
+    segundo_digito_informado = int(cpf[10])
 
-    for indice, numero_cpf in enumerate(cpf, start=10):
+    for indice, numero_cpf in enumerate(cpf_numeros, start=10):
         lista_cpf.append(numero_cpf)
         lista_contagem.append(contagem)
         contagem -= 1
@@ -31,7 +36,7 @@ while True:
     lista_cpf = []
 
     # Inclui os 9 dígitos + o primeiro dígito verificador
-    cpf_com_primeiro_digito = cpf + str(primeiro_digito)
+    cpf_com_primeiro_digito = cpf_numeros + str(primeiro_digito)
     
     for indice, numero_cpf in enumerate(cpf_com_primeiro_digito):
         lista_cpf.append(numero_cpf)
@@ -49,8 +54,15 @@ while True:
         segundo_digito = 11 - (resultado_multiplicacao % 11)
         print(f'O segundo digito verificador é: {segundo_digito}')
 
-    # print(*lista_contagem)
-    # print(*lista_cpf)
-    print ('Seu cpf completo é: ' + cpf + str(primeiro_digito) + str(segundo_digito))
-    #print(f'Resultado da multiplicação: {resultado_multiplicacao}')
+    # Validação do CPF
+    print(f'\n--- Validação ---')
+    print(f'Seu CPF completo é: {cpf}')
+    print(f'Dígitos informados: {primeiro_digito_informado} e {segundo_digito_informado}')
+    print(f'Dígitos calculados: {primeiro_digito} e {segundo_digito}')
+    
+    if primeiro_digito == primeiro_digito_informado and segundo_digito == segundo_digito_informado:
+        print('✓ CPF VÁLIDO!')
+    else:
+        print('✗ CPF INVÁLIDO!')
+    
     break

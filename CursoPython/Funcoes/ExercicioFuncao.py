5866bed363675fc8f1855f70433ab365b10c4a9b
brenoscominato <brenocominato@hotmail.com>
2026-06-13 23:46:48 -0300

    funcao

---
+++ CursoPython/Funcoes/ExercicioFuncao.py
@@ -0,0 +1,11 @@
+def multiplica(*args):
+    contador = 1
+    for numeros in args:
+        print('Conta', contador, '*', numeros)
+        contador = contador * numeros
+        print('Resultado Conta: ', contador)
+    return contador
+    
+numeracao = 1, 7, 9
+resultado_real = multiplica(*numeracao)
+print(resultado_real)

10a785fb39868b838f15df631c04ba9f7d41fd94
brenoscominato <brenocominato@hotmail.com>
2026-06-15 21:38:45 -0300

    Funcao

--- CursoPython/Funcoes/ExercicioFuncao.py
+++ CursoPython/Funcoes/ExercicioFuncao.py
@@ -1,3 +1,4 @@
+""""
 def multiplica(*args):
     contador = 1
     for numeros in args:
@@ -9,3 +10,18 @@ def multiplica(*args):
 numeracao = 1, 7, 9, 10
 resultado_real = multiplica(*numeracao)
 print(resultado_real)
+"""
+
+def multiplicacao(numero):
+    divisiveis_por_dois = numero % 2 == 0
+
+    if divisiveis_por_dois:
+        return f'{numero} é par'
+    return f'{numero} é impar'
+
+print(multiplicacao(3))
+print(multiplicacao(40))
+print(multiplicacao(37))
+print(multiplicacao(355))
+print(multiplicacao(6))
+
